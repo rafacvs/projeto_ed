fa110d9ababc65ab0b13b739836b2b9e2f48968f
rafacvs <rafa.cvs1@hotmail.com>
2022-04-14 16:29:23 -0300

    passing functions to utils

--- utils.h
+++ utils.h
@@ -4,8 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "lista.h"
+#include "pilha.h"
+
+typedef struct {
+  lista mao;
+  int soma;
+  int fichas;
+  int aposta_atual;
+  int gameover;
+} PERSON;
+
 int getValue(char card, int aceValue);
 void swap(int v[], int a, int b);
 void shuffle(int vetor[], int n);
+void comprarCarta(pilha *p, PERSON *jogador, int mesa);
+void jogadaMesa(PERSON jogadores[], int qtdJogadores, pilha *p);
+void initializePlayers(int qtdJogadores, PERSON jogadores[qtdJogadores]);
+void firstRound(pilha p, int qtdJogadores, PERSON jogadores[qtdJogadores]);
+void rounds(pilha p, int qtdJogadores, PERSON jogadores[qtdJogadores]);
+void summary(int qtdJogadores, PERSON jogadores[qtdJogadores], PERSON mesa);
 
 #endif
