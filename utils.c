7112fa619b8429c15f4acfca993f374aee4c68c2
rafacvs <rafa.cvs1@hotmail.com>
2022-04-13 21:58:45 -0300

    passing functions to utils

--- utils.c
+++ utils.c
@@ -1,8 +1,8 @@
 #include "utils.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 
 int getValue(char card, int aceValue) {
   switch (card) {
@@ -65,5 +65,131 @@ void shuffle(int vetor[], int n) {
     int j = i + rand() % (n - i);
 
     swap(vetor, i, j);
+  }
+}
+
+void comprarCarta(pilha *p, PERSON *jogador) {
+  int aceValue = -1;
+  char option;
+
+  REG reg;
+
+  topoPilha(p, &reg.chave);
+
+  if (reg.chave == 'X') {
+    printf("carta = 10\n");
+  } else {
+    printf("carta = %c\n", reg.chave);
+  }
+
+  if (reg.chave == 65) {
+    printf("A vale 11? (S ou N)\n");
+    scanf(" %c", &option);
+
+    if (option == 'S') {
+      aceValue = 1;
+    } else {
+      aceValue = 0;
+    }
+  }
+
+  jogador->soma += getValue(reg.chave, aceValue);
+
+  insertLista(&jogador->mao, reg);
+
+  removePilha(p);
+}
+
+void initalizeGame(int qtdJogadores, PERSON jogadores[qtdJogadores], pilha p) {
+  printf("=========== DISTRIBUINDO CARTAS ===========\n\n");
+  // ROUND INICIAL: 2 CARTAS PARA CADA JOGAODR E MESA
+  for (int i = 0; i < qtdJogadores; i++) {
+    if (i == qtdJogadores - 1) {
+      printf("MESA\n");
+    } else {
+      printf("JOGADOR %i\n", i + 1);
+    }
+    printf("--------------\n");
+    if (i != qtdJogadores - 1) {
+      while (1) {
+        printf("quanto quer apostar?\n");
+        scanf("%i", &jogadores[i].aposta_atual);
+
+        if (jogadores[i].aposta_atual > jogadores[i].fichas) {
+          printf("kk pobre\n");
+        } else {
+          break;
+        }
+      }
+
+      jogadores[i].fichas -= jogadores[i].aposta_atual;
+    }
+
+    for (int j = 0; j < 2; j++) {
+      comprarCarta(&p, &jogadores[i]);
+    }
+
+    printf("soma = %i\n\n", jogadores[i].soma);
+
+    printf("\n");
+  }
+}
+
+void round(int qtdJogadores, PERSON jogadores[qtdJogadores], pilha p) {
+  printf("=========== JOGADAS INVIDUAIS ===========\n\n");
+  // ROUNDS
+  for (int i = 0; i < qtdJogadores - 1; i++) {
+    int stop = 0;
+    char option;
+
+    printf("JOGADOR %i\n", i + 1);
+    printf("-----------\n");
+
+    while (stop == 0) {
+      if (jogadores[i].soma > 21) {
+        jogadores[i].gameover = 1;
+        stop = 1;
+        break;
+      }
+
+      printf("fichas atuais: %i\n\n", jogadores[i].fichas);
+      printf("aposta: %i\n", jogadores[i].aposta_atual);
+      printf("soma atual: %i\n\n", jogadores[i].soma);
+
+      printf("O que voce deseja?\n");
+      printf("  Comprar mais ou parar? (C ou P)\n");
+
+      scanf(" %c", &option);
+      if (option == 'C') {
+        comprarCarta(&p, &jogadores[i]);
+        printf("\n");
+      } else {
+        stop = 1;
+      }
+    }
+  }
+}
+
+void summary(int qtdJogadores, PERSON jogadores[qtdJogadores], PERSON mesa) {
+  // RESUMO DA PARTIDA
+  printf("\n\n======== RESUMO DA PARTIDA ========\n\n");
+  for (int i = 0; i < qtdJogadores - 1; i++) {
+    printf("JOGADOR %i\n", i + 1);
+    printf("-----------\n");
+
+    if (jogadores[i].gameover == 1) {
+      printf("PERDESTES!\n");
+    } else {
+      if (jogadores[i].soma > mesa.soma) {
+        printf("ganhou mt bom\n");
+      } else if (jogadores[i].soma < mesa.soma) {
+        printf("\nPERDESTES!\n");
+      } else {
+        printf("empate - fichas devolvidas\n");
+        jogadores[i].fichas += jogadores[i].aposta_atual;
+        jogadores[i].aposta_atual = 0;
+      }
+    }
+    printf("\n\n");
   }
 }
--- utils.h
+++ utils.h
@@ -4,7 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "lista.h"
+#include "pilha.h"
+
+typedef struct {
+  lista mao;
+  int soma;
+  int fichas;
+  int aposta_atual;
+  int gameover;
+} PERSON;
+
 int getValue(char card, int aceValue);
 void shuffle(int vetor[], int n);
+void comprarCarta(pilha *p, PERSON *jogador);
+void initalizeGame(int qtdJogadores, PERSON jogadores[qtdJogadores], pilha p);
+void round(int qtdJogadores, PERSON jogadores[qtdJogadores], pilha p);
+void summary(int qtdJogadores, PERSON jogadores[qtdJogadores], PERSON mesa);
 
 #endif
